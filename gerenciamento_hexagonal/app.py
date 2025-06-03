11147e5c64186785f4f2fa95c7808badd59305c6
João Martins <joaodetona@hotmail.com>
2025-06-02 23:01:06 -0300

    allowed origins

--- gerenciamento_hexagonal/app.py
+++ gerenciamento_hexagonal/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from gerenciamento_hexagonal.entrypoints.routers.arquivo import router as arquivo_router
 from gerenciamento_hexagonal.entrypoints.routers.gerenciamento_caracterizacao import router as gerenciamento_caracterizacao_router
@@ -12,6 +13,18 @@ from gerenciamento_hexagonal.entrypoints.routers.gerenciamento_quantitativo impo
 from gerenciamento_hexagonal.entrypoints.routers.relatorio import router as relatorio_router
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000", "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+
 
 app.include_router(relatorio_router, prefix='/Relatorio', tags=['Relatorio'])
 app.include_router(gerenciamento_proposta_router, prefix='/GerenciamentoProposta', tags=['Gerenciamento Proposta'])
